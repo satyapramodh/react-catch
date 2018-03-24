12fae15c242c2144cdb829a22d18768ae3771fd7
Pramodh Mazumdar <satyapramodh@gmail.com>
2018-03-23 21:30:54 -0700

    add fish form, inventory and app components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,14 +5,27 @@ import Order from "./Order";
 
 
 class App extends React.Component{
+  state = {
+    fishes: {},
+    order: {}
+  };
+
+  addFish = (fish) => {
+    const fishes = {...this.state.fishes};
+    fishes[`fish${Date.now()}`] = fish;
+    this.setState({ fishes });
+  };
+
   render() {
-    return <div className="catch-of-the-day">
+    return (
+      <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market" />
         </div>
-        <Inventory />
         <Order />
-      </div>;
+        <Inventory addFish={this.addFish} />
+      </div>
+    )
   }
 }
 
--- src/components/StorePicker.jsx
+++ src/components/StorePicker.jsx
@@ -1,12 +1,25 @@
 import React, {Fragment} from 'react';
+import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+  myInput = React.createRef();
+
+  goToStore = (event) => {
+    event.preventDefault();
+
+    const storeName = this.myInput.value.value;
+    this.props.history.push(`/store/${storeName}`);
+  }
+
   render() {
     return (
       <Fragment>
         {/* This is a store selector */}
-        <form action="" className="store-selector">
-          <input type="text" required placeholder="Store Name"/>
+        <form className="store-selector" onSubmit={this.goToStore}>
+          <input type="text" required
+          placeholder="Store Name"
+          defaultValue={getFunName()}
+          ref={this.myInput} />
           <button type="submit">Visit Store</button>
         </form>
       </Fragment>
