78009e30ccad649be8ddc8bca2d07b58de4f8289
Pramodh Mazumdar <satyapramodh@gmail.com>
2018-03-23 22:25:40 -0700

    create fish list

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import Header from './Header';
 import Inventory from "./Inventory";
 import Order from "./Order";
-
+import sampleFishes from '../sample-fishes';
+import Fish from './Fish';
 
 class App extends React.Component{
   state = {
@@ -16,16 +17,23 @@ class App extends React.Component{
     this.setState({ fishes });
   };
 
+  loadSampleFishes = () => {
+    this.setState({ fishes: sampleFishes });
+  }
+
   render() {
-    return (
-      <div className="catch-of-the-day">
+    return <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market" />
+          <ul>
+            {Object.keys(this.state.fishes).map(key => (
+              <Fish key={key} details={this.state.fishes[key]} />
+            ))}
+          </ul>
         </div>
         <Order />
-        <Inventory addFish={this.addFish} />
-      </div>
-    )
+        <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes} />
+      </div>;
   }
 }
 
